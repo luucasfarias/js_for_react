690cbb5e873805a731ac50576f00eff8b525f87f
lucas.farias <lucas.farias@db1.com.br>
2021-07-12 20:35:36 -0300

    :tada: Create a cardGame component

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.figma.com/file/k8GPJJNlqcjXvnF6Nmk2XL/Matching-game?node-id=1%3A129
+
+https://meyerweb.com/eric/tools/css/reset/
+
+Gitmoji: https://gitmoji.dev
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img src="assets/images/alura-pixel.png" alt="logo alura">
+    </article>
+  `;
+}
+
+export default CardGame;
